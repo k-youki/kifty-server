d8a573db9b98fe27483653308ee8085309c870ec
Yuki Kotani <Yuki@Kotanikis-Mac.local>
2018-05-19 14:30:33 +0900

    first commit

---
+++ app/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+//    return view('welcome');
+    return "Hello World";
+});
+
+//Route::get('/spotify', 'App\Controllers\SpotifyContoller@index');
+Route::get('spotify', function () {
+    return "Spotify";
+});
